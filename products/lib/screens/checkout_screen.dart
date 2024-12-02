2d1b39f3eb0b4553a1a003445d86cbed41caad6d
Ayoub Hach <ayoubleonhach@gmail.com>
2024-12-02 14:02:46 +0100

    second commit

--- products/lib/screens/checkout_screen.dart
+++ products/lib/screens/checkout_screen.dart
@@ -11,73 +11,68 @@ class CheckoutScreen extends StatelessWidget {
     final cartViewModel = Provider.of<CartViewModel>(context);
 
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: const Text('Checkout'),
-    ),
-    body: Padding(
-    padding: const EdgeInsets.all(16.0),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-    const Text(
-    'Order Summary',
-    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-    ),
-    const SizedBox(height: 10),
-    ListView.builder(
-    shrinkWrap: true,
-    itemCount: cartViewModel.cartItems.length,
-    itemBuilder: (context, index) {
-    final cartItem = cartViewModel.cartItems[index];
-    return ListTile(
-    leading: Image.network(cartItem.product.thumbnail),
-    title: Text(cartItem.product.title),
-    subtitle: Text('Quantity: ${cartItem.quantity}'),
-    trailing: Text('\$${cartItem.totalPrice}'),
-    );
-    },
-    ),
-    const SizedBox(height: 20),
-    Text(
-    'Total Price: \$${cartViewModel.totalPrice}',
-    style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-    ),
-    const SizedBox(height: 20),
-    ElevatedButton(
-    onPressed: () async {
-    // Simulate sending order data with a mock API POST request
-    if (kDebugMode) {
-      print('Order sent: ${cartViewModel.cartItems}');
-    }
-    showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-    title: const Text('Order Placed'),
-    content: const Text('Your order has been placed successfully!'),
-    actions: [
-    TextButton(
-    onPressed: () {
-    Navigator.pop(context);
-    cartViewModel.cartItems.clear();
-    Navigator.pushNamedAndRemoveUntil(
-        context,
-        '/',
-        ModalRoute.withName("/")
-    );
-    },
-    child: const Text('OK'),
-    ),
-    ],
-    ),
-    );
-    },
-    child: const Text('Place Order'),
-    ),
-    ],
-    ),
-    ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Order Summary',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: cartViewModel.cartItems.length,
+              itemBuilder: (context, index) {
+                final cartItem = cartViewModel.cartItems[index];
+                return ListTile(
+                  leading: Image.network(cartItem.product.thumbnail),
+                  title: Text(cartItem.product.title),
+                  subtitle: Text('Quantity: ${cartItem.quantity}'),
+                  trailing: Text('\$${cartItem.totalPrice}'),
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Total Price: \$${cartViewModel.totalPrice}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                if (kDebugMode) {
+                  print('Order sent: ${cartViewModel.cartItems}');
+                }
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: const Text('Order Placed'),
+                    content:
+                        const Text('Your order has been placed successfully!'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          cartViewModel.cartItems.clear();
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, '/', ModalRoute.withName("/"));
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: const Text('Place Order'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
-
--- products/lib/services/api_service.dart
+++ products/lib/services/api_service.dart
@@ -5,16 +5,14 @@ import 'package:products/models/product.dart';
 import 'package:products/constants.dart';
 
 class ApiService {
-  // Fetch products from the API
   Future<List<Product>> fetchProducts() async {
     try {
       final response = await http.get(Uri.parse(Constants.baseUrl));
 
-      // Check if the API call was successful (status code 200)
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body)['products'];
 
-        // Map the response to a list of Product objects
+
         return data.map((product) => Product.fromJson(product)).toList();
       } else {
         throw Exception('Failed to load products');
