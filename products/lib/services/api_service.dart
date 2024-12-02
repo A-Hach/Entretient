aa2f771b635bf2ab51eb872f6a31ff52c05a4d9a
Ayoub Hach <ayoubleonhach@gmail.com>
2024-12-02 14:10:18 +0100

    second commit

--- products/lib/services/api_service.dart
+++ products/lib/services/api_service.dart
@@ -5,26 +5,14 @@ import 'package:products/models/product.dart';
 import 'package:products/constants.dart';
 
 class ApiService {
-<<<<<<< HEAD
-=======
-
->>>>>>> f3621ee0d0760a70a25265bb5b9b2d10f7198bf4
   Future<List<Product>> fetchProducts() async {
     try {
       final response = await http.get(Uri.parse(Constants.baseUrl));
 
-<<<<<<< HEAD
       if (response.statusCode == 200) {
         final List<dynamic> data = json.decode(response.body)['products'];
 
 
-=======
-     
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body)['products'];
-
-        
->>>>>>> f3621ee0d0760a70a25265bb5b9b2d10f7198bf4
         return data.map((product) => Product.fromJson(product)).toList();
       } else {
         throw Exception('Failed to load products');
